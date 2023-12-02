ca9cba452a67195946223d43105c84725c2946ca
JayTalekar <jaytalekar.developer@gmail.com>
2023-12-01 19:22:17 -0500

    Basic Layout of the App

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,135 @@
+// React and Router hooks
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import {Routes, Route, NavLink, Link} from 'react-router-dom'
+
+//Mui Components
+import Box from '@mui/material/Box';
+import Drawer from '@mui/material/Drawer';
+import AppBar from '@mui/material/AppBar';
+import CssBaseline from '@mui/material/CssBaseline';
+import Toolbar from '@mui/material/Toolbar';
+import List from '@mui/material/List';
+import Typography from '@mui/material/Typography';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+
+//Mui Icons
+import DashboardIcon from '@mui/icons-material/Dashboard';
+import AnalyticsIcon from '@mui/icons-material/Analytics';
+import ContactsIcon from '@mui/icons-material/Contacts';
+import InsertDriveFileIcon from '@mui/icons-material/InsertDriveFile';
+import TuneIcon from '@mui/icons-material/Tune';
+
+//Importing all Pages
+import {DashBoardPage}  from './components/DashboardPage';
+import {StatisticsPage} from './components/StatisticsPage';
+import {ContactsPage} from './components/ContactsPage';
+import {DocumentsPage} from './components/DocumentsPage'
+import {SettingsPage} from './components/SettingsPage'
+
+const drawerWidth = 200
+const dashboard = 'Dashboard'
+const statistics = 'Statistics'
+const contacts = 'Contacts'
+const documents = 'Documents'
+const settings = 'Settings'
 
 function App() {
-  const [count, setCount] = useState(0)
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+  return(
+    <Box sx={{display: 'flex'}}>
+      <CssBaseline/>
+      <AppBar position="fixed" sx={{ zIndex: (theme) => theme.zIndex.drawer + 1 }}>
+
+        <Toolbar variant="dense" sx={{display:'flex', flexDirection: 'row'}}>
+          
+          <Typography sx={{ width: 200}} variant="h6" color="inherit">
+            Job Journey
+          </Typography>
+
+          <Typography sx={{flexGrow: 5}} variant="h5" color="inherit">
+              Job Hunt 2023
+          </Typography>
+
+          <Typography variant='h7' >
+            Jay Talekar
+          </Typography>
+          
+        </Toolbar>
+      </AppBar>
+      
+      <Drawer
+        variant="permanent"
+        sx={{
+          width: drawerWidth,
+          flexShrink: 0,
+          [`& .MuiDrawer-paper`]: { width: drawerWidth, boxSizing: 'border-box' },
+        }}
+      >
+        <Toolbar />
+        <Box sx={{ overflow: 'auto' }}>
+          <List>
+            <ListItem key={dashboard} disablePadding>
+              <ListItemButton component={NavLink} to={"/"}>
+                <ListItemIcon>
+                    <DashboardIcon/>
+                </ListItemIcon>
+                <ListItemText primary={dashboard}/>
+              </ListItemButton>
+            </ListItem>
+
+            <ListItem key={statistics} disablePadding>
+              <ListItemButton component={NavLink} to={"/statistics"}>
+                <ListItemIcon>
+                    <AnalyticsIcon/>
+                </ListItemIcon>
+                <ListItemText primary={statistics}/>
+              </ListItemButton>
+            </ListItem>
+            
+            <ListItem key={contacts} disablePadding>
+              <ListItemButton component={NavLink} to={"/contacts"}>
+                <ListItemIcon>
+                    <ContactsIcon/>
+                </ListItemIcon>
+                <ListItemText primary={contacts}/>
+              </ListItemButton>
+            </ListItem>
+
+            <ListItem key={documents} disablePadding>
+              <ListItemButton component={NavLink} to={"/documents"}>
+                <ListItemIcon>
+                    <InsertDriveFileIcon/>
+                </ListItemIcon>
+                <ListItemText primary={documents}/>
+              </ListItemButton>
+            </ListItem>
+
+            <ListItem key={settings} disablePadding>
+              <ListItemButton component={NavLink} to={"/settings"}>
+                <ListItemIcon>
+                    <TuneIcon/>
+                </ListItemIcon>
+                <ListItemText primary={settings}/>
+              </ListItemButton>
+            </ListItem>
+
+          </List>
+        </Box>
+      </Drawer>
+
+      <Box sx={{marginTop: "50px", marginX: "20px"}}>
+        <Routes>
+          <Route path={"/"} element={<DashBoardPage/>} />
+          <Route path={"/statistics"} element={<StatisticsPage/>} />
+          <Route path={"/contacts"} element={<ContactsPage/>} />
+          <Route path={"/documents"} element={<DocumentsPage/>} />
+          <Route path={"/settings"} element={<SettingsPage/>} />
+        </Routes>
+      </Box>
+
+    </Box>
   )
 }
 
---
+++ src/components/DashboardPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export const DashBoardPage = () => {
+  return (
+    <div>
+      <h1>Dashboard Page</h1>
+    </div>
+  );
+};
