4163782f267adfeb6ffd22fcdd4ccbb075d6d93f
JayTalekar <jaytalekar.developer@gmail.com>
2023-12-16 00:45:45 -0500

    Update firebase config

--- src/firebase/FirebaseConfig.js
+++ src/firebase/FirebaseConfig.js
@@ -1,9 +1,9 @@
 export default {
-    apiKey: import.meta.env.VITE_FIREBASE_KEY,
-    authDomain: import.meta.env.VITE_FIREBASE_DOMAIN,
-    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: import.meta.env.VITE_FIREBASE_SENDER_ID,
-    appId: import.meta.env.VITE_FIREBASE_APP_ID,
-    measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
+    apiKey: process.env.VITE_FIREBASE_KEY,
+    authDomain: process.env.VITE_FIREBASE_DOMAIN,
+    projectId: process.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: process.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.VITE_FIREBASE_SENDER_ID,
+    appId: process.env.VITE_FIREBASE_APP_ID,
+    measurementId: process.env.VITE_FIREBASE_MEASUREMENT_ID
   };
